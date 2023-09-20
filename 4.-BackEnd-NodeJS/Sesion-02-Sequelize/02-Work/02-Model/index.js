1816cd3672fc2c98866459fad93c8fe2b66c0f84
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-09-19 21:18:01 -0600

    Commit para subir cambios hechos en sesiones

--- 4.-BackEnd-NodeJS/Sesion-02-Sequelize/02-Work/02-Model/index.js
+++ 4.-BackEnd-NodeJS/Sesion-02-Sequelize/02-Work/02-Model/index.js
@@ -19,14 +19,78 @@ async function connect() {
 
 }
 connect();
-
+/*
+  CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    views INTEGER DEFAULT 0 NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE NOT NULL
+  )
+ */
 const Task = sequelize.define("tasks", {
   name: {
     type: DataTypes.STRING(100),
     allowNull: false
   },
   description: {
-  type: DataTypes.STRING(),
-  allowNull: false
-  }
+    type: DataTypes.STRING(),
+    allowNull: false
+  },
+  views: {
+		type: DataTypes.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+	isActive: {
+		field: "is_active",
+		type: DataTypes.BOOLEAN,
+		defaultValue: true,
+		allowNull: false,
+	},
 });
+
+async function sync() {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("> Base de datos actualizada!")
+  }catch(e) {
+    console.error("> No se pudo conectar..");
+    console.error(e);
+  }
+}
+sync();
+
+/*const Task = sequelize.define("tasks", {
+	name: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+	description: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	views: {
+		type: DataTypes.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+	isActive: {
+		field: "is_active",
+		type: DataTypes.BOOLEAN,
+		defaultValue: true,
+		allowNull: false,
+	},
+});
+
+async function sync() {
+	try {
+		await sequelize.sync({ force: true });
+		console.log("> Base de datos actualizada!");
+	} catch (e) {
+		console.error("> No se puede actualizar la base de datos");
+		console.error(e);
+	}
+}
+
+sync();*/
